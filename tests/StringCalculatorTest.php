fa2705d259cbdacde47cc12a0872f3e2708a0f2c
Jörg Basedow <jbasedow@mindworks.de>
2013-09-23 10:30:51 +0200

    Refactored tests to use data provider

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -4,30 +4,24 @@ require_once('../src/StringCalculator.php');
 
 class StringCalculatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAddWithEmptyString()
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testAddWithEmptyString($input, $exprected)
     {
         $calculator = new StringCalculator();
 
-        $result = $calculator->add('');
+        $result = $calculator->add($input);
 
-        $this->assertSame(0, $result);
+        $this->assertSame($exprected, $result);
     }
 
-    public function testAddWithOneNumber()
+    public function dataProvider()
     {
-        $calculator = new StringCalculator();
-
-        $result = $calculator->add('7');
-
-        $this->assertSame(7, $result);
-    }
-
-    public function testAddWithTwoNumber()
-    {
-        $calculator = new StringCalculator();
-
-        $result = $calculator->add('4,6');
-
-        $this->assertSame(10, $result);
+        return array(
+            array('', 0),
+            array('7', 7),
+            array('4,6', 10),
+        );
     }
 }
